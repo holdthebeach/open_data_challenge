8d05b1e9f94055eccf918c7590baa120f91c06d2
Patty <patty@Patricias-MacBook-Pro.local>
2023-10-03 16:34:46 +0200

    create react project with vite and update icon and title of app

---
+++ open-data-app/src/App.tsx
@@ -0,0 +1,20 @@
+import openDataLogo from './assets/icon.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <a href="/" target="_blank">
+          <img src={openDataLogo} className="logo" alt="Open Data App logo" />
+        </a>
+      </div>
+      <h1>Open Data App</h1>
+
+    </>
+  )
+}
+
+export default App
